323c59f4d709c106287607f18543cc04405cea36
SiyanPu <pusiyan0117@163.com>
2021-10-17 21:47:33 -0400

    image session update

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,9 +1,10 @@
 import { Image } from "./image";
 import {Button} from 'react-bootstrap';
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AddPost from './addPost';
 import Layout from "react-photo-gallery";
 import { photos } from "./photos";
+import Axios from "axios";
 
 function columns(containerWidth) {
   let columns = 1;
@@ -16,6 +17,17 @@ function columns(containerWidth) {
 
 export function Gallery(props){
   const [addState,setAddState]=useState(false);
+  const [isLoggedIn, setLoggedIn]=useState(false);
+  const [loggedInUser,setLoggedInUser]=useState('');
+  useEffect(()=>{
+    Axios.get("/api/login").then((response)=>{
+      if(response.data.loggedIn==true){
+        console.log(response.data.user);
+        setLoggedIn(true);
+        setLoggedInUser(response.data.user.username);
+      };
+    })
+  },[]);
   return (
     <div>
     <div id='portfolio' className='text-center'>
@@ -37,7 +49,7 @@ export function Gallery(props){
             {props.data
               ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
-                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} loggedIn={isLoggedIn}/>
                 </div>
               ))
               : 'Loading...'}
