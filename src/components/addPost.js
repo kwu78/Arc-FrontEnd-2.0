08eadb7917cd211dc747944a21d6768625d06c2b
zackkkkkz <zackzhang1228@gmail.com>
2021-10-22 14:36:12 -0400

    add post update

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -1,14 +1,39 @@
 import React, { useState,useEffect, useContext } from "react";
-import {Modal,FloatingLabel,Form,Row,Col,ToggleButton,ButtonGroup} from 'react-bootstrap';
+import {Modal,FloatingLabel,Form,Row,Col,ToggleButton,ButtonGroup,Container} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import Axios from "axios";
 import {useHistory} from "react-router-dom";
+import addPost from "./addPost.css"
+
+
 function AddPost(props){
   const [artType,setartType]=useState('artwork');
   const [photoURL,setPhotoURL]=useState(null);
   const [isLoggedIn, setLoggedIn]=useState(false);
-  const [loggedInUser,setLoggedInUser]=useState('');
+  const [loggedInUser,setLoggedInUser]=useState('')
   const history=useHistory();
+
+
+  const [selectedImage, setSelectedImage] = useState();
+  const removeSelectedImage = () => {setSelectedImage();};
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
+  };
+
+
+  // constructor(props){
+  //   super(props)
+  //   this.state = {file: null}
+  //   this.handleChange = this.handleChange.bind(this)
+  //   }
+    // handleChange(event) {
+    //   this.setState({
+    //     file: URL.createObjectURL(event.target.files[0])
+    //   })
+    // }
+
   useEffect(()=>{
     Axios.get("/api/login").then((response)=>{
       if(response.data.loggedIn==true){
@@ -27,7 +52,7 @@ function AddPost(props){
     setPhotoURL(URL.createObjectURL(e.target.files[0]));
   }
   function upload(){
-   
+
     let post={
       title:document.getElementById('photoID').value,
       description:document.getElementById('description').value,
@@ -38,7 +63,7 @@ function AddPost(props){
     console.log(post);
     Axios.post('/posts',post).then(function(response){
       console.log(response);
-      
+
     });
     props.onHide();
     history.push("/");
@@ -50,65 +75,101 @@ function AddPost(props){
     animation={false}
     centered
   >
-    <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add A New Post
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Upload Your Art Here</h4>
-        <FloatingLabel  controlId="floatingTextarea" label="Title" className="mb-3">
-    <Form.Control id='photoID' as="textarea" placeholder="Leave a comment here" />
-  </FloatingLabel>
-  <FloatingLabel controlId="floatingTextarea2" label="Description">
-    <Form.Control
-      id='description'
-      as="textarea"
-      placeholder="Leave a comment here"
-      style={{ height: '100px' }}
-    />
-  </FloatingLabel>
-  <fieldset>
-    <Form.Group as={Row} className="mb-3">
-        <Form.Check
+  <Container>
+    <Row>
+      <Col>
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter" >
+              Add A New Post
+            </Modal.Title>
+          </Modal.Header>
+
+            <h4>Upload Your Art Here</h4>
+      </Col>
+      <Col>
+      <FloatingLabel  controlId="floatingTextarea" label="Title" className="mb-3">
+        <Form.Control id='photoID' as="textarea" placeholder="Leave a comment here" />
+      </FloatingLabel>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <ButtonGroup className="mb-2">
+        {type.map((choice) => (
+          <ToggleButton
+          key={choice.id}
+          id={`radio-${choice.id}`}
           type="radio"
-          label="Upload an Artwork"
-          name="artwork"
-          id="artwork"
-        />
-        <Form.Check
-          type="radio"
-          label="Upload a Photography"
-          name="photography"
-          id="photography"
-        />
-    </Form.Group>
-    <ButtonGroup className="mb-2">
-    {type.map((choice) => (
-    <ToggleButton
-            key={choice.id}
-            id={`radio-${choice.id}`}
-            type="radio"
-            variant='outline-primary'
-            name="artType"
-  
-            value={choice.value}
-            checked={artType === choice.value}
-            onChange={(e) => setartType(e.currentTarget.value)}
-          >
-            {choice.value}
+          variant='outline-primary'
+          name="artType"
+
+          value={choice.value}
+          checked={artType === choice.value}
+          onChange={(e) => setartType(e.currentTarget.value)}
+        >
+          {choice.value}
           </ToggleButton>
-          ))}
-          </ButtonGroup>
-    <Form.Group>
-    <input type="file" id='uploadedWork' className="form-control" onChange={uploadImage} />
-    </Form.Group>
-  </fieldset>
-      </Modal.Body>
-    <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-        <Button onClick={upload} type='submit'>Upload</Button>
-      </Modal.Footer>
+            ))}
+        </ButtonGroup>
+      </Col>
+      <Col>
+        <FloatingLabel controlId="floatingTextarea2" label="Description">
+          <Form.Control
+            id='description'
+            as="textarea"
+            placeholder="Leave a comment here"
+            style={{ height: '100px' }}
+          />
+        </FloatingLabel>
+      </Col>
+    </Row>
+    <Row>
+      <fieldset>
+        <Form.Group as={Col} className="mb-3">
+            <Form.Check
+              type="radio"
+              label="Upload an Artwork"
+              name="artwork"
+              id="artwork"
+            />
+            <Form.Check
+              type="radio"
+              label="Upload a Photography"
+              name="photography"
+              id="photography"
+            />
+            </Form.Group>
+      </fieldset>
+    </Row>
+    <div claaName = "container">
+        <input
+          accept="image/*"
+          type="file"
+          onChange={imageChange}
+        />
+          {selectedImage && (
+            <div className = "preview">
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                className = "image"
+                alt="Thumb"
+              />
+              <button onClick={removeSelectedImage} className = "delete">
+                Remove This Image
+              </button>
+            </div>
+          )}
+    </div>
+
+    <Row>
+      <Col>
+              <Button onClick={props.onHide}>Close</Button>
+      </Col>
+      <Col md = 'auto'>
+              <Button onClick={upload} type='submit'>Upload</Button>
+      </Col>
+    </Row>
+  </Container>
    </Modal>);
 }
 export default AddPost;
