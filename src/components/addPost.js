41f35e9b4bc1b82f74287f9e431d18e3e2b2cda5
Kevin Wu <kwu78@emory.edu>
2021-10-24 17:10:04 -0400

    updated for leo

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -7,14 +7,16 @@ import { FilePond, File, registerPlugin,Create } from 'react-filepond'
 import 'filepond/dist/filepond.min.css'
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview,FilePondPluginFileEncode)
 function AddPost(props){
   const [artType,setartType]=useState('artwork');
   const [photoURL,setPhotoURL]=useState(null);
   const [isLoggedIn, setLoggedIn]=useState(false);
   const [loggedInUser,setLoggedInUser]=useState('');
   const [files, setFiles] = useState([]);
+  const[clicked,setClicked]=useState(false);
   const history=useHistory();
   useEffect(()=>{
     Axios.get("/api/login").then((response)=>{
@@ -51,6 +53,7 @@ function AddPost(props){
       userinfo:loggedInUser._id
     }
     console.log(post);
+    setClicked(true);
     Axios.post('/posts',post).then(function(response){
       console.log(response);
     });
@@ -120,8 +123,57 @@ function AddPost(props){
         files={files}
         onupdatefiles={setFiles}
         allowMultiple={true}
+        server={{
+          url:'/posts/temp',
+          timeout:7000,
+          process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+            // fieldName is the name of the input field
+            // file is the actual file object to send
+            const formData = new FormData();
+            const id=1234;
+            formData.append(fieldName, file, file.name);
+            console.log(formData);
+            const request = new XMLHttpRequest();
+            request.open('POST', '/posts/temp');
+            console.log(request);
+            console.log(fieldName);
+            console.log(file);
+            // Should call the progress method to update the progress to 100% before calling load
+            // Setting computable to false switches the loading indicator to infinite mode
+            request.upload.onprogress = (e) => {
+                progress(e.lengthComputable, e.loaded, e.total);
+            };
+            
+            // Should call the load method when done and pass the returned server file id
+            // this server file id is then used later on when reverting or restoring a file
+            // so your server knows which file to return without exposing that info to the client
+            request.onload = function () {
+                if (request.status >= 200 && request.status < 300) {
+                    // the load method accepts either a string (id) or an object
+                    load(request.responseText);
+                } else {
+                    // Can call the error method if something is wrong, should exit after
+                    error('oh no');
+                }
+            };
+            console.log(formData);
+            request.send(formData);
+
+            // Should expose an abort method so the request can be cancelled
+            return {
+                abort: () => {
+                    // This function is entered if the user has tapped the cancel button
+                    request.abort();
+
+                    // Let FilePond know the request has been cancelled
+                    abort();
+                },
+            };
+        },
+          load:"./upload",
+          fetch:'./upload'
+        }}
         maxFiles={3}
-        server="/posts"
         name="files"
         labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
       />
